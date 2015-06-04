e7db6136bdfba7e76d10db097c4e00a070a7aa2a
Felipe Barbirato <fbarbirato@gmail.com>
2015-06-03 23:28:46 -0300

    Adding DataGateways proj

---
+++ Bills.Application/Bills.Application.DataGateways/IBillDataGateway.cs
@@ -0,0 +1,17 @@
+﻿using Bills.Application.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bills.Application.DataGateways
+{
+    public interface IBillDataGateway
+    {
+        void AddNew(Bill bill);
+        
+        void Save(Bill bill);
+
+        Bill FindBy(int id);
+    }
+}
